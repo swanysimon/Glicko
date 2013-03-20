083f14b0946d2e6842b5fce13d302ad0ad011cdd
Simon Swanson <nomiswanson@gmail.com>
2013-03-20 10:12:26 -0500

    Update Parser.hs

--- Parser.hs
+++ Parser.hs
@@ -1,28 +1,35 @@
 -- Parser.hs
 -- Simon Swanson
 -- takes in the HTML files and parses, writing out to file
--- currently only works for basketball
+-- currently only works for basketball and baseball (be careful with Spring Training)
 
 module Main where
 
 import Rating
 import System.IO
-import System.Environment (getArgs, getProgName)
-import Network.HTTP (simpleHTTP, getResponseBody, getRequest)
+import System.IO.Strict as Strict
+import System.Environment
+import Network.HTTP
 import Text.HTML.TagSoup
 import Data.List
 import Data.Char
 import Text.Regex.Posix
-import Data.Time.Clock (getCurrentTime, utctDay)
-import Data.Time.Calendar (diffDays, addDays, fromGregorian, toGregorian, Day)
+import Data.Time.Clock
+import Data.Time.Calendar
 import Data.Text (split, pack, unpack)
 
 
 -- because of the way ~== and =~ operate I have to create my own dropWhile function
 scoreDrop :: [Tag String] -> [Tag String]
 scoreDrop xs | null xs = []
-    | head xs ~== TagOpen "div" [] && fromAttrib "id" (head xs) =~ "scores-[0-9]+" = xs
+    | head xs ~== TagOpen "div" [] && fromAttrib "class" (head xs) =~ "spanCol[0-9]?" = xs
     | otherwise = scoreDrop (tail xs)
+
+-- roughly the end of the scoreboard for both mlb and nba
+taken :: [Tag String] -> [Tag String]
+taken xs | null xs = []
+    | head xs ~== TagOpen "div" [] && fromAttrib "class" (head xs) =~ "score[a-zA-z]*Key" = []
+    | otherwise = (head xs) : taken (tail xs)
 
 
 -- retrieves games that have gone final
@@ -34,32 +41,45 @@ final xs | null xs = []
 
 -- breaks up the information until all that is left is the team name, its record, and, if it won, an indicator saying it won
 segments :: String -> [[[String]]]
-segments = text . isolate . extras . map (splitTeam) . final . parts . ftaken . scoreDrop . parseTags where
-    ftaken = takeWhile (/= TagOpen "div" [("class", "scoreboardKey")])
+segments = doubleheader . text . isolate . extras . map (splitTeam) . final . parts . taken . scoreDrop . parseTags where
     parts = partitions (~== TagOpen "table" [("class", "lineScore")])
     splitTeam = partitions (~== TagOpen "tr" [("id", "final"), ("align", "right")])
     extras xs | null xs = []
-        | otherwise = map (takeWhile (/= TagOpen "td" [("align","center")])) (head xs) : extras (tail xs)
+        | otherwise = map (takeWhile (/= TagClose "td")) (head xs) : extras (tail xs)
     isolate xs | null xs =[]
         | otherwise = map (filter isTagText) (head xs) : isolate (tail xs)
     text xs | null xs = []
         | otherwise = map (map fromTagText) (head xs) : text (tail xs)
+    doubleheader xs | null xs = []
+        | (head xs) `elem` (tail xs) = (map decrease (head xs)) : doubleheader (tail xs)
+        | otherwise = (head xs) : doubleheader (tail xs)
+    decrease xs = case xs of
+        [] -> []
+        _ -> (head xs ++ "1") : drop 1 xs
 
 
--- takes in a teams record and turns it into the number of played games
-gameEval :: String -> Int
-gameEval = sum . makenum . map unpack . split (== '-') . pack . filter simDigit where
-    makenum x = map read x :: [Int]
-    simDigit n = (n >= '0' && n <= '9') || (n == '-')
+-- takes in a team's record and turns it into the number of played games
+gameEval :: String -> String -> Int
+gameEval team s | simfilter team == team = supersum s
+    | otherwise = supersum s - 1 where
+        supersum = sum . makenum . map unpack . split (== '-') . pack . filter simDigit
+        makenum x = map read x :: [Int]
+        simDigit n = (n >= '0' && n <= '9') || (n == '-')
 
 
 -- cleans up the lists to prepare for the final execution, with each team now represented by [name, gamesplayed, gameresult]
 clear :: [[String]] -> [[String]]
 clear xs | null xs = []
     | xs == [[]] = []
-    | (length $ head xs) == 3 = [[(head . head) xs, (show . gameEval) ((head xs) !! 1), "w"], [(head . last) xs, (show . gameEval) ((last xs) !! 1), "l"]]
-    | (length $ last xs) == 3 = [[(head . head) xs, (show . gameEval) ((head xs) !! 1), "l"], [(head . last) xs, (show . gameEval) ((last xs) !! 1), "w"]]
-    | otherwise = [[(head . head) xs, (show . gameEval) ((head xs) !! 1), "d"], [(head . last) xs, (show . gameEval) ((last xs) !! 1), "d"]]
+    | (length $ head xs) == 3 = [[simfilter $ (head . head) xs, (show . gameEval (head (head xs))) ((head xs) !! 1), "w"], [simfilter $ (head . last) xs, (show . gameEval (head (head xs))) ((last xs) !! 1), "l"]]
+    | (length $ last xs) == 3 = [[simfilter $ (head . head) xs, (show . gameEval (head (head xs))) ((head xs) !! 1), "l"], [simfilter $ (head . last) xs, (show . gameEval (head (head xs))) ((last xs) !! 1), "w"]]
+    | otherwise = [[simfilter $ (head . head) xs, (show . gameEval (head (head xs))) ((head xs) !! 1), "d"], [simfilter $ (head . last) xs, (show . gameEval (head (head xs))) ((last xs) !! 1), "d"]]
+
+
+simfilter :: String -> String
+simfilter s | null s = ""
+    | last s == '1' = init s
+    | otherwise = s
 
 
 getTeams :: [Glicko] -> [[[String]]] -> [Glicko]
@@ -70,66 +90,41 @@ getTeams gs xs | null xs = gs
         rs = newList gs (head $ head $ head xs) gs
 
 
--- updates the Glicko for each team in a game
-updateGlicko :: [[[String]]] -> [Glicko] -> [Glicko]
-updateGlicko xs gs | null xs = gs
-    | xs == [[]] = []
-    | xs == [[[]]] = []
-    | tup2 ((head . res . glicko1 . head) xs) == games head (head xs) = updateGlicko (tail xs) gs
-    | otherwise = updateGlicko (tail xs) (new (head xs)) where
-        glicko1 = findTeam gs . head . head
-        glicko2 = findTeam gs . head . last
-        games f ns = read (((f) ns) !! 1) :: Int
-        team1 ns = Glicko {
-            team = team $ glicko1 ns,
-            rating = rating $ glicko1 ns,
-            dev = dev $ glicko1 ns,
-            time = time $ glicko1 ns,
-            res = (last $ head ns, games head ns, [rating $ glicko2 ns, dev $ glicko2 ns]) : (res $ glicko1 ns)
-        }
-        team2 ns = Glicko {
-            team = head $ last ns,
-            rating = rating $ glicko2 ns,
-            dev = dev $ glicko2 ns,
-            time = time $ glicko2 ns,
-            res = (last $ last ns, games last ns, [rating $ glicko1 ns, dev $ glicko1 ns]) : (res $ glicko2 ns)
-        }
-        new rs = upList (team2 rs) (upList (team1 rs) gs)
-
-
 -- takes in the current date, the starting date, the league name, all the teams for that league, and the day for evaluating
 -- writes all undocumented games to file, updating ratings as necessary
 checkEval :: Day -> Day -> String -> [Glicko] -> Day -> IO ()
 checkEval curDate oldDate league teams eval = case diffDays oldDate eval of
     -- if it's the day after the designated evaluation date (to make sure every game finishes), evaluates before adding in any new games
         1 -> do
-            putStrLn $ "Ratings updated as of " ++ (show eval) ++"."
+            putStrLn $ "Ratings updated as of " ++ (showGregorian eval) ++"."
             run curDate oldDate league (map (runGlicko league) teams) (addDays (n league) eval)
         _ -> run curDate oldDate league teams eval
 
 
 run :: Day -> Day -> String -> [Glicko] -> Day -> IO ()
 run curDate oldDate league teams eval = do
-    putStrLn $ "Processing games from " ++ (show oldDate) ++ "."
-    http <- simpleHTTP (getRequest $ "http://www.cbssports.com/" ++ league ++ "/scoreboard/" ++ filter (/= '-') (show oldDate)) >>= getResponseBody
+    putStrLn $ "Processing games from " ++ (showGregorian oldDate) ++ "."
+    http <- simpleHTTP (getRequest $ "http://www.cbssports.com/" ++ league ++ "/scoreboard/" ++ filter (/= '-') (showGregorian oldDate)) >>= getResponseBody
     let fscores = map clear $ segments http
-    case fscores of
-        [] -> putStrLn "Done."
-        _ -> do
-            let newteams = (reverse . sort . updateGlicko fscores) (getTeams teams fscores)
-            case diffDays curDate oldDate of
-                
-                0 -> do
-                    let path = "/Users/Swanson/Programs/Haskell/glicko-ratings/glicko" ++ league
-                    writeFile path $ (show . toGregorian) curDate ++ "\n" ++ show newteams ++ "\n" ++ (show . toGregorian) eval
-                    putStrLn $ "Current " ++ league ++ " rankings:"
-                    prettyprint newteams where
-                        prettyprint xs | null xs = return ()
-                            | otherwise = do
-                                putStrLn $ (team . head) xs
-                                prettyprint (tail xs)
 
-                _ -> checkEval curDate (addDays 1 oldDate) league newteams eval
+    case fscores of
+        [] -> checkDate curDate oldDate league teams eval
+        _ -> checkDate curDate oldDate league (updateGlicko fscores (getTeams teams fscores)) eval
+
+
+checkDate :: Day -> Day -> String -> [Glicko] -> Day -> IO ()
+checkDate curDate oldDate league teams eval = case curDate `compare` oldDate of
+    EQ -> do
+        let newCur = show $ toGregorian curDate
+        let newEval = show $ toGregorian eval
+        let newTeam = reverse $ sort $ teams
+        writeFile ("/Users/Swanson/Programs/Haskell/glicko-ratings/glicko" ++ league) (newCur ++ "\n" ++ (show newTeam) ++ "\n" ++ newEval)
+        prettyprint newTeam where
+            prettyprint xs | null xs = return ()
+                | otherwise = do
+                    putStrLn $ show (rating (head xs)) ++ " - " ++ team (head xs)
+                    prettyprint (tail xs)
+    _ -> checkEval curDate (addDays 1 oldDate) league teams eval
 
 
 main :: IO ()
@@ -140,7 +135,8 @@ main = do
         1 -> do
             curDate <- getCurrentTime >>= return . utctDay
             let league = map toLower (head args)
-            fp <- readFile ("/Users/Swanson/Programs/Haskell/glicko-ratings/glicko" ++ league) >>= return . lines
+            -- strict file reading because Haskell hates the world and I don't have time to figure out ByteStrings right now (which are apparently strict)
+            fp <- Strict.readFile ("/Users/Swanson/Programs/Haskell/glicko-ratings/glicko" ++ league) >>= return . lines
             let e = read $ head fp :: (Integer, Int, Int)
             let oldDate = fromGregorian (tup1 e) (tup2 e) (tup3 e)
             let d = read (fp !! 2) :: (Integer, Int, Int)
@@ -148,7 +144,18 @@ main = do
             let teams = read (fp !! 1) :: [Glicko]
             checkEval curDate oldDate league teams eval
 
+        3 -> do
+            let c = read (last args) :: (Integer, Int, Int)
+            let curDate = fromGregorian (tup1 c) (tup2 c) (tup3 c)
+            let g = read (args !! 1) :: (Integer, Int, Int)
+            let oldDate = fromGregorian (tup1 g) (tup2 g) (tup3 g)
+            let league = map toLower (head args)
+            fp <- Strict.readFile ("/Users/Swanson/Programs/Haskell/glicko-ratings/glicko" ++ league) >>= return . lines
+            let eval = addDays (n league) oldDate
+            let teams = read (fp !! 1) :: [Glicko]
+            putStrLn $ "Evaluating from " ++ (showGregorian oldDate) ++ " to " ++ (showGregorian curDate) ++ ". Understand that this may create problems in files and should be used only for testing purposes."
+            checkEval curDate oldDate league teams eval       
+
         _ -> do
             prog <- getProgName
             putStrLn $ "Usage: " ++ (show prog) ++ " leaguename"
-
